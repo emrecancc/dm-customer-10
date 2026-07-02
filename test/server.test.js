442ca20ec231209706d7fd0335ed4657d8f11ea9
emrecancc <emrecancerli55@gmail.com>
2026-07-03 00:46:06 +0300

    fix: ensure server is closed after tests

--- test/server.test.js
+++ test/server.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
 const app = require('../src/app');
-const server = app.listen(3059);
+const server = app.listen(3119);
 
 describe('Server', () => {
   it('responds to GET /', async () => {
-    const res = await request(app).get('/');
+    const res = await request(server).get('/');
     expect(res.statusCode).toBe(200);
   });
 });
