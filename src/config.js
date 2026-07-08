ee659c41d3b980049fbe266db5bba4f33445a755
emrecancc <emrecancerli55@gmail.com>
2026-07-09 02:33:53 +0300

    ci: introduce syn_js (s8)

--- src/config.js
+++ src/config.js
@@ -1,12 +1,7 @@
-// src/config.js
-const process = require('process');
-
-function validateConfig() {
-  const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET || 'dev-secret';
-  if (!NEXTAUTH_SECRET) {
-    throw new Error('NEXTAUTH_SECRET environment variable is required but not set.');
-  }
-  // other config validations can be added here
-}
-
-module.exports = { validateConfig };
+const config = {
+  port: 3000,
+  host: 'localhost'
+  debug: true,
+  timeout: 5000
+};
+module.exports = config;
